a3f5de1f11838b6e4c4fc9ceeae2400b2bbc13bc
evanpy <evanleewj@gmail.com>
2022-07-05 20:34:47 +0800

    Revert "Added bot and backend model with REST api"
    This reverts commit 1d257cd85fa57a7b6c547e268b9781835be3651e.

--- bot/apps.py
+++ bot/apps.py
@@ -1,11 +1,6 @@
-import os
 from django.apps import AppConfig
-# from .bot import main
+
 
 class BotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot'
-
-    # def ready(self):
-    #     if os.environ.get('RUN_MAIN', None) != 'true':
-    #         main()
